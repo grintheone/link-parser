e02cb5664df1961d9c4453e4c046b3d678c43f40
grintheone <grin3run@gmail.com>
2025-06-17 11:04:21 +0300

    feat: working with all example files

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/grintheone/link-parser
 
 go 1.24.3
+
+require golang.org/x/net v0.41.0 // indirect
--- main.go
+++ main.go
@@ -1,4 +1,68 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type Link struct {
+	Href string
+	Text string
+}
+
+func walk(n *html.Node, links *[]Link) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		var l Link
+		extractText(n, &l)
+
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				l.Href = a.Val
+				break
+			}
+		}
+
+		*links = append(*links, l)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		walk(c, links)
+	}
+}
+
+func extractText(n *html.Node, l *Link) {
+	if n.Type == html.TextNode {
+		l.Text += " " + strings.TrimSpace(n.Data)
+		l.Text = strings.TrimSpace(l.Text)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		extractText(c, l)
+	}
+}
+
 func main() {
+	var links []Link
+	fname := flag.String("fname", "", "specify the name of the file to open")
+	flag.Parse()
+
+	f, err := os.Open(*fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	doc, err := html.Parse(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	walk(doc, &links)
+
+	fmt.Printf("Links after a walk %#v", links)
 }
